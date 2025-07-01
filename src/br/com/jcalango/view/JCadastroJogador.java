4c318c8c83afff734b1c5da754e5e7b2acd7e3f6
iara <iaraotako@gmail.com>
2025-07-01 09:17:12 -0300

    JCalangoApp Atualizado

--- src/br/com/jcalango/view/JCadastroJogador.java
+++ src/br/com/jcalango/view/JCadastroJogador.java
@@ -7,39 +7,54 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-//extends é herança 
-
-public class JCadastroJogador extends JFrame{
-    //Construtor inicializa os componentes do formulario
-    public JCadastroJogador(){
-        //Titulo da janela
-        setTitle("Faça parte do JKalango!");
+// extends é herança
+public class JCadastroJogador extends JFrame {
+    //Construtor inicializa os componentes do formulário
+    public JCadastroJogador() {
+        // Titulo da janela
+        setTitle("Faça parte do JKalango ");
+        // Garante que a aplicação não seja finalizada
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //TAMANHO
-        setSize(400,450);
-        //posição
+        // tamanho da janela
+        setSize(400, 400);
+        // posição ao centro
         setLocationRelativeTo(null);
         setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
-        JLabel lblnome = new JLabel("nome");
-        JTextField txtNome = new JTextField(25);
-        add(lblnome);
+        JLabel lblNome = new JLabel("Nome");
+        JTextField txtNome = new JTextField(30);
+        add(lblNome);
         add(txtNome);
-     JButton btnCadatrar = new JButton("Cdastrar");
-     btnCadatrar.addActionListener(new ActionListener() {
+        JLabel lblNickname = new JLabel("Nickname");
+        JTextField txtNickname = new JTextField(30);
+        add(lblNickname);
+        add(txtNickname);
+        JLabel lblEmail = new JLabel("Email");
+        JTextField txtEmail = new JTextField(30);
+        add(lblEmail);
+        add(txtEmail);
+        JLabel lblTelefone = new JLabel("Telefone");
+        JTextField txtTelefone = new JTextField(30);
+        add(lblTelefone);
+        add(txtTelefone);
+        JLabel lblSenha = new JLabel("Senha");
+        JTextField txtSenha = new JTextField(30);
+        add(lblSenha);
+        add(txtSenha);
+        JButton btnCadastrar = new JButton("Cadastrar");
+        btnCadastrar.addActionListener(new ActionListener(){
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-        }
-        
-     });
-     setVisible(true);
-
-        
-
-}
-
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                dispose();
+                JOptionPane.showMessageDialog(null, "Jogador Cadastrado com Sucesso!");
+            }
+            
+        });
+        setVisible(true);
+        add(btnCadastrar);
+    }
 }
--- src/br/com/jcalango/view/JIntro.java
+++ src/br/com/jcalango/view/JIntro.java
@@ -1,38 +1,52 @@
 package br.com.jcalango.view;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class JIntro extends JFrame {
-    public JIntro() {
-        setTitle("JKalango INTRO");
-        setSize(600, 300);
+    public JIntro(){
+        setTitle("JKalango Splash Screen");
+        setSize(900, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new FlowLayout());
 
-        String narracao = "Sua memória é um borrão... tudo começou com o frio cortante, a névoa espessa...\n"
+        String textoIntro ="Sua memória é um borrão... tudo começou com o frio cortante, a névoa espessa...\n"
         + "E aquela voz distante, quase um sussurro, chamando você pelo nome, como se já te conhecesse.\n"
         + "A casa diante de você não é apenas velha — ela respira, observa.\n"
         + "À medida que você atravessa seus corredores e abre cada porta, ela parece se lembrar... de quem você realmente é.";
 
-        
-        JTextArea textoPanic = new JTextArea(narracao);
-        add(textoPanic, BorderLayout.CENTER);
-        JButton startJButton = new JButton("Iniciar");
-        add(startJButton);
-        startJButton.addActionListener(new ActionListener() {
-            
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                new JCadastroJogador();
-                 JOptionPane.showMessageDialog(JIntro.this, "PRIMEIRA PARTE ");
-            }
-        });
 
+        JLabel janela = new JLabel(textoIntro);
+        janela.setOpaque(false);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        panel.add(janela);
+
+        JButton startButton = new JButton("Iniciar Missão");
+        panel.add(startButton);
+        
+        startButton.addActionListener(new ActionListener() {
+        @Override
+        
+        public void actionPerformed(ActionEvent e) {
+            //JOptionPane.showMessageDialog(JIntro.this, "Primeira Missão Iniciada");
+            dispose();
+            new JCadastroJogador();
+        }
+        });
+        setContentPane(panel);
         setVisible(true);
     }
+   public static void main(String[] args) {
+    new JIntro();
+   }
 }
